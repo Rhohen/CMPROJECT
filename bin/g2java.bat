a9b5bf237a0934cdd6bc09ea590db1a913d38f85
Rhohen <nico.giro72@gmail.com>
2018-02-15 14:55:37 +0100

    project finally working

--- bin/g2java.bat
+++ bin/g2java.bat
@@ -2,7 +2,7 @@
 :: appel par g2java nom-de-votre-grammaire-suffixe-par-g
 
 :: Commande si antlr-3.5.2-complete.jar est celui du share
-java -cp  N:\l3info\comp\antlr\antlr-3.5.2-complete.jar org.antlr.Tool %*
+java -cp  H:\workspace\CMPROJECT\antlr\antlr-3.5.2-complete.jar org.antlr.Tool %*
 
 :: Commande si antlr-3.5.2-complete.jar EST COPIE SOUS VOTRE REPERTOIRE
 ::    -> pensez alors a indiquer le chemin correct
